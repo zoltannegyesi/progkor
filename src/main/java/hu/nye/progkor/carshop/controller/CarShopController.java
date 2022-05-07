a22367cc4515ca2b837d326c13829e09f010550f
Négyesi Zoltán <zoltannegyesi4@gmail.com>
2022-05-07 13:43:19 +0200

    CarShopController

---
+++ src/main/java/hu/nye/progkor/carshop/controller/CarShopController.java
@@ -0,0 +1,25 @@
+package hu.nye.progkor.carshop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import hu.nye.progkor.carshop.model.Car;
+import hu.nye.progkor.carshop.service.CarShopService;
+import reactor.core.publisher.Flux;
+
+@Controller
+@RequestMapping(value = "/car-shop")
+public class CarShopController {
+    
+    private final CarShopService carShopService;
+
+    @Autowired
+    public CarShopController(CarShopService carShopService) {
+        this.carShopService = carShopService;
+    }
+
+    
+}
