d99d6d6c15fc353e4d831d42662df1bb01247383
Négyesi Zoltán <zoltannegyesi4@gmail.com>
2022-05-07 11:30:09 +0200

    Car

---
+++ src/main/java/hu/nye/progkor/carshop/model/Car.java
@@ -0,0 +1,16 @@
+package hu.nye.progkor.carshop.model;
+
+public class Car {
+    
+    Long id;
+
+    String brand;
+
+    String model;
+
+    Double displacement;
+
+    Integer hoursepower;
+
+    Fuel fuel;
+}
